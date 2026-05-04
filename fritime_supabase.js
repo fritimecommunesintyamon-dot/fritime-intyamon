fdc14261bb96a9024e1aa13b51d5c605a54af6bb
fritimecommunesintyamon-dot <fritimecommunesintyamon@gmail.com>
2026-05-04 20:36:12 +0200

    Add files via upload

--- fritime_supabase.js
+++ fritime_supabase.js
@@ -59,7 +59,7 @@ const FriDB = {
     var allowed = ['nom','description','date','date_limite','heure_debut','heure_fin','lieu',
       'prest_id','age_min','age_max','places','inscrits','surveillants','cout','cout_type',
       'cout_facture','materiel','tenue','statut','annee','categorie',
-      't_public','t_parent','t_meteo','t_service','notes'];
+      't_public','t_parent','t_meteo','t_service','notes','image'];
     var clean = {};
     allowed.forEach(function(k){ if(k in a && a[k] !== undefined && a[k] !== null) clean[k] = a[k]; });
     if (a.id) {
